c62750aed017ce132f8c5b603eedd39aefab3477
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-03-31 16:46:55 -0300

    fix

--- Queries/salesQueries.js
+++ Queries/salesQueries.js
@@ -1,20 +1,20 @@
 const getAll = `SELECT t1.id, t1.date, t2.product_id, t2.quantity 
-FROM StoreManager.sales AS t1 
-inner join StoreManager.sales_products AS t2 ON t1.id = t2.sale_id;`;
+FROM heroku_09f15076813b26b.sales AS t1 
+inner join heroku_09f15076813b26b.sales_products AS t2 ON t1.id = t2.sale_id;`;
 
 const getById = `SELECT t1.date, t2.product_id, t2.quantity 
-FROM StoreManager.sales AS t1
-INNER JOIN StoreManager.sales_products AS t2
+FROM heroku_09f15076813b26b.sales AS t1
+INNER JOIN heroku_09f15076813b26b.sales_products AS t2
 ON t1.id = t2.sale_id
 WHERE id=?
 ORDER BY t2.sale_id ASC, t2.product_id ASC;`;
 
-const createSaleRecordId = 'INSERT INTO StoreManager.sales () VALUES ();';
+const createSaleRecordId = 'INSERT INTO heroku_09f15076813b26b.sales () VALUES ();';
 
-const createNewSale = `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
-VALUES (?, ?, ?);`;
+const createNewSale = `INSERT INTO heroku_09f15076813b26b.sales_products
+(sale_id, product_id, quantity) VALUES (?, ?, ?);`;
 
-const update = `UPDATE StoreManager.sales_products
+const update = `UPDATE heroku_09f15076813b26b.sales_products
 SET quantity=? WHERE product_id=? AND sale_id=?;`;
 
 module.exports = {
