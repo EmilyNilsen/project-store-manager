681d4145703bda17407c4325dce8395aff1f0bd2
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-03-31 09:59:41 -0300

    conectando banco com app

--- models/connection.js
+++ models/connection.js
@@ -2,9 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  HOST: 'us-cdbr-east-05.cleardb.net',
+  USER: 'bbf8351727d70b',
+  PASSWORD: '965d7ed4',
+  DB: 'heroku_09f15076813b26b',
 });
 
 module.exports = connection;
