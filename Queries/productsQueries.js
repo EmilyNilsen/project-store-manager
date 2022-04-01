8671baadc326fc63283f22a6a657a9da51b2812c
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-04-01 09:12:54 -0300

    fix

--- Queries/productsQueries.js
+++ Queries/productsQueries.js
@@ -6,9 +6,9 @@ const getById = `SELECT * FROM ${DB}.products WHERE id=?;`;
 
 const create = `INSERT INTO ${DB}.products (name, quantity) VALUES (?, ?);`;
 
-const update = `UPDATE ${DB}..products SET name=?, quantity=? WHERE id=?;`;
+const update = `UPDATE ${DB}.products SET name=?, quantity=? WHERE id=?;`;
 
-const deleteProduct = `DELETE FROM ${DB}..products WHERE id=?;`;
+const deleteProduct = `DELETE FROM ${DB}.products WHERE id=?;`;
 
 module.exports = {
   getAll,
--- services/productsServices.js
+++ services/productsServices.js
@@ -16,7 +16,7 @@ const findById = async (id) => {
 const createNewProduct = async ({ name, quantity }) => {
   const getProducts = await getAll();
   if (getProducts.some((obj) => obj.name === name)) return null;
-  const newProduct = await ProductModel.create({ name, quantity });
+  const newProduct = await ProductModel.createNewProduct({ name, quantity });
   return newProduct;
 };
 
@@ -24,7 +24,7 @@ const updateProduct = async ({ id, name, quantity }) => {
   const products = await getAll();
   const intId = parseInt(id, 10);
   if (!products.some((obj) => obj.id === intId)) return null;
-  const updatedProduct = await ProductModel.update({ id, name, quantity });
+  const updatedProduct = await ProductModel.updateProduct({ id, name, quantity });
   return updatedProduct;
 };
 
