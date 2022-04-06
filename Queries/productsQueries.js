d608e35933e715cb1e47648443a968cc54c0bf0b
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-04-05 21:25:57 -0300

    corrigido erro de indentacao e alterado nomes de variaveis de ambiente para evitar conflitos com variaveis do SO

--- Queries/productsQueries.js
+++ Queries/productsQueries.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-const getAll = `SELECT * FROM ${process.env.DB}.products ORDER BY id ASC;`;
+const getAll = `SELECT * FROM ${process.env.DB_NAME}.products ORDER BY id ASC;`;
 
-const getById = `SELECT * FROM ${process.env.DB}.products WHERE id=?;`;
+const getById = `SELECT * FROM ${process.env.DB_NAME}.products WHERE id=?;`;
 
-const create = `INSERT INTO ${process.env.DB}.products (name, quantity) VALUES (?, ?);`;
+const create = `INSERT INTO ${process.env.DB_NAME}.products (name, quantity) VALUES (?, ?);`;
 
-const update = `UPDATE ${process.env.DB}.products SET name=?, quantity=? WHERE id=?;`;
+const update = `UPDATE ${process.env.DB_NAME}.products SET name=?, quantity=? WHERE id=?;`;
 
-const deleteProduct = `DELETE FROM ${process.env.DB}.products WHERE id=?;`;
+const deleteProduct = `DELETE FROM ${process.env.DB_NAME}.products WHERE id=?;`;
 
 module.exports = {
   getAll,
--- Queries/salesQueries.js
+++ Queries/salesQueries.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 const getAll = `SELECT t1.id, t1.date, t2.product_id, t2.quantity 
-FROM ${process.env.DB}.sales AS t1 
-inner join ${process.env.DB}.sales_products AS t2 ON t1.id = t2.sale_id;`;
+FROM ${process.env.DB_NAME}.sales AS t1 
+inner join ${process.env.DB_NAME}.sales_products AS t2 ON t1.id = t2.sale_id;`;
 
 const getById = `SELECT t1.date, t2.product_id, t2.quantity 
-FROM ${process.env.DB}.sales AS t1
-INNER JOIN ${process.env.DB}.sales_products AS t2
+FROM ${process.env.DB_NAME}.sales AS t1
+INNER JOIN ${process.env.DB_NAME}.sales_products AS t2
 ON t1.id = t2.sale_id
 WHERE id=?
 ORDER BY t2.sale_id ASC, t2.product_id ASC;`;
 
-const createSaleRecordId = `INSERT INTO ${process.env.DB}.sales () VALUES ();`;
+const createSaleRecordId = `INSERT INTO ${process.env.DB_NAME}.sales () VALUES ();`;
 
-const createNewSale = `INSERT INTO ${process.env.DB}.sales_products
+const createNewSale = `INSERT INTO ${process.env.DB_NAME}.sales_products
 (sale_id, product_id, quantity) VALUES (?, ?, ?);`;
 
-const update = `UPDATE ${process.env.DB}.sales_products
+const update = `UPDATE ${process.env.DB_NAME}.sales_products
 SET quantity=? WHERE product_id=? AND sale_id=?;`;
 
-const deleteAllSaleRecords = `DELETE FROM ${process.env.DB}.sales WHERE id=?;`;
+const deleteAllSaleRecords = `DELETE FROM ${process.env.DB_NAME}.sales WHERE id=?;`;
 
 module.exports = {
   getAll,
