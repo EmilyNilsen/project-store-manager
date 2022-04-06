3f51a6a006c6928b31a11461cce8263052671368
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-04-06 18:42:13 -0300

    resolvendo novos codesmells

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,6 +1,6 @@
 const ProductModel = require('../models/productsModels');
 
-const getAll = async () => await ProductModel.getAll();
+const getAll = async () => ProductModel.getAll();
 
 const findById = async (id) => {
   const product = await ProductModel.getById(id);
@@ -13,20 +13,20 @@ const findById = async (id) => {
 const createNewProduct = async ({ name, quantity }) => {
   const getProducts = await getAll();
   if (getProducts.some((obj) => obj.name === name)) return null;
-  return await ProductModel.createNewProduct({ name, quantity });
+  return ProductModel.createNewProduct({ name, quantity });
 };
 
 const updateProduct = async ({ id, name, quantity }) => {
   const products = await getAll();
   const intId = parseInt(id, 10);
   if (!products.some((obj) => obj.id === intId)) return null;
-  return await ProductModel.updateProduct({ id, name, quantity });
+  return ProductModel.updateProduct({ id, name, quantity });
 };
 
 const deleteProduct = async ({ id }) => {
   const product = await findById(id);
   if (!product) return null;
-  return await ProductModel.deleteProduct(id);
+  return ProductModel.deleteProduct(id);
 };
 
 module.exports = {
